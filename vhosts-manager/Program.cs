ed372c7af8d788ddb776479215a08417c922be84
KOMP\gladius882 <p.spychalski@ntiinnovations.com>
2018-07-06 20:01:39 +0200

    Empty  WinForms project

---
+++ vhosts-manager/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: gladius882
+ * Date: 2018-07-06
+ * Time: 20:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace vhosts_manager
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
